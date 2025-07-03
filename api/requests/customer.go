2cc46e8334b11365007db3f6c7c5357b8a669d72
achmadardian <achmadardian37@gmail.com>
2025-07-03 22:27:10 +0700

    feat(requests): add customer request

---
+++ api/requests/customer.go
@@ -0,0 +1,17 @@
+package requests
+
+type CreateCustomerRequest struct {
+	NationalityID int    `json:"nationality_id"`
+	CstName       string `json:"cst_name"`
+	CstDOB        string `json:"cst_dob"`
+	CstPhoneNum   string `json:"cst_phone_num"`
+	CstEmail      string `json:"cst_email"`
+}
+
+type UpdateCustomerRequest struct {
+	NationalityID int    `json:"nationality_id,omitempty"`
+	CstName       string `json:"cst_name,omitempty"`
+	CstDOB        string `json:"cst_dob,omitempty"`
+	CstPhoneNum   string `json:"cst_phone_num,omitempty"`
+	CstEmail      string `json:"cst_email,omitempty"`
+}
